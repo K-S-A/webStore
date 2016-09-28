8883c03f59fc4ce3a7e8f31f9978f5c2cf7fe05b
K-S-A <atillas@ukr.net>
2016-09-29 01:49:44 +0300

    Fix n+1 query for stories; fix css for scrollbar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.root_categories
+    @categories = Category.root_categories.includes(subcategories: [subcategories: [subcategories: :subcategories]])
   end
 
   def show
