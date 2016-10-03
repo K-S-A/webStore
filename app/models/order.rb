b7282bca2a2f0e49ff06637f68d188bb5286d39b
K-S-A <atillas@ukr.net>
2016-10-03 09:50:02 +0300

    Update Order model; add base mailer views

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,13 @@ class Order < ActiveRecord::Base
   accepts_nested_attributes_for :order_items
 
   default_scope { includes(order_items: :product) }
+
+  before_validation :set_stock_number
+
+  private
+
+  def set_stock_number
+    self.stock_number = Order.maximum(:stock_number).next
+  end
+
 end
